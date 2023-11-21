93e09c00337f0a3e433cb784875c73c7de5d63c8
JoanWaithira <joanwaithira.jw@gmail.com>
2023-11-21 00:50:53 -0800

    Correcting task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,11 @@
 -- A script that prepares a MYSQL server for the project
 -- A database of name hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Created a user with password hbnb_dev_pwd in localhost
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant all pviledges for the hbnb_dev on hbnb_dev_db.
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
 -- Grants SELECT privilege for hbnb_dev on performance_schema.
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
