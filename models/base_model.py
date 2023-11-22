4b32827b92b63aad547f3e6d4f30b805872e625d
JoanWaithira <joanwaithira.jw@gmail.com>
2023-11-22 02:43:14 -0800

    Create Base = declarative_base()

--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,16 @@ from datetime import datetime
 from os import getenv
 from sqlalchemy import Column, Integer, String, Datetime
 
+Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
