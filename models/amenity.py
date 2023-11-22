1308b3b85c19434e9784860d12e93a90abde5943
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-11-22 00:50:10 +0000

    Added sqlalchemy modifications for amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+
 """ State Module for HBNB project """
+
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
-    name = ""
+    "Amenities available in the airbnb"
+
+    if storageType == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
