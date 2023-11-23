996edc6e0d230aa8bae90be91b2ce4305f0e99f6
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-23 05:56:40 +0300

    Hot fix to amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python3.8
 """ State Module for HBNB project """
-
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, storageType
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    "Amenities available in the airbnb"
-
+class Amenity(BaseModel, Base):
+    """Amenity Class"""
     if storageType == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
