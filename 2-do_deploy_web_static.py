1d64a748ed84efd02871ed3a55bdae19bff6ffa4
JoanWaithira <joanwaithira.jw@gmail.com>
2023-12-11 01:28:33 -0800

    Do deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3i
+# distributes an archive to your web servers, using the function do_deploy
+
+
+from fabric.api import run, put, env
+from os.path import exists
+from datetime import datetime
+
+env.hosts = ['54.82.179.171', '100.25.132.241']
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to a web server
+    """
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
