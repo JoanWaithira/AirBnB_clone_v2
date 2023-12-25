dda33f0b487650d57355c3978b5e926e710680db
JoanWaithira <joanwaithira.jw@gmail.com>
2023-12-25 13:32:36 -0800

    Pycodestyle

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,8 +45,8 @@ def display_number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def display_number_odd_or_even(n):
     """Displays an HTML page if n is an integer"""
-    odd_or_even = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', n=n, odd_even=odd_or_even)
+    odd_eve = "even" if n % 2 == 0 else "odd"
+    return render_template('6-number_odd_or_even.html', n=n, odd_even=odd_eve)
 
 
 if __name__ == '__main__':
